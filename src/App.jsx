22677ca29cc317eb5443b841acb6d3b08aaf414d
Tsany-student <tsani1209@student.abudzar.sch.id>
2025-10-21 09:46:33 +0700

    update hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 // import './App.css'
 
-import Header from './components/Header/Header'
-import Herosection from './components/Herosection/Herosection'
-import Navigasi from './components/Navigasi/Navigasi'
+import Header from './components/Header/Header';
+import Herosection from './components/Herosection/Herosection';
+import Navigasi from './components/Navigasi/Navigasi';
 
 function App() {
   const [count, setCount] = useState(0)
